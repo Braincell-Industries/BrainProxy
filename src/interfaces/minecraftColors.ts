7483847f25c927f9550bcc28dd82a3216b25dd05
ItzMarni <72508916+ItzMarni@users.noreply.github.com>
2023-10-05 19:20:08 +0200

    I'm stupid and forgot to save 2 files...

--- src/interfaces/minecraftColors.ts
+++ src/interfaces/minecraftColors.ts
@@ -1,6 +1,7 @@
 // Define an interface for Minecraft colors.
 export interface MinecraftColor {
   mcName: string;
+  beautifiedName: string;
   colorCode: string;
   modernColorCode: string;
   ansiEscapeCode: string;
